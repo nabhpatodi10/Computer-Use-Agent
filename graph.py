dc93fa0437e5db518909bd19c82e2bf73c66a654
nabhpatodi10 <nabhpatodi1005@gmail.com>
2025-06-07 01:33:44 +0530

    Refactor Graph class to use ChatOpenAI; update model initialization and enhance __plan and __agent methods to handle image processing with base64 encoding for screenshots.

--- graph.py
+++ graph.py
@@ -1,4 +1,4 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openai import ChatOpenAI
 from langgraph.graph import StateGraph, END
 from langchain_core.messages import AnyMessage, HumanMessage
 from typing import Annotated, TypedDict
@@ -6,6 +6,7 @@ import operator
 import time
 from PIL import ImageGrab
 import base64
+from openai import RateLimitError
 
 from agent import Agent
 from nodes import Nodes
@@ -15,13 +16,12 @@ from windows import Screen, Mouse, Keyboard
 class GraphState(TypedDict):
     task: str
     plan: Plan
-    steps_completed: Annotated[list[str], operator.add]
     messages: Annotated[list[AnyMessage], operator.add]
     replan: Replan
 
 class Graph:
 
-    def __init__(self, model: ChatGoogleGenerativeAI = ChatGoogleGenerativeAI(model = "models/gemini-2.0-flash")):
+    def __init__(self, model: ChatOpenAI = ChatOpenAI(model = "gpt-4.1-nano")):
 
         __graph = StateGraph(GraphState)
         __graph.add_node("plan_node", self.__plan)
@@ -29,7 +29,7 @@ class Graph:
         __graph.add_node("replan_node", self.__replan)
         __graph.add_edge("plan_node", "agent")
         __graph.add_edge("agent", "replan_node")
-        __graph.add_conditional_edges("replan_node", self.__check_end, {True : "agent", False : END})
+        __graph.add_conditional_edges("replan_node", self.__check_end, {False : "agent", True : END})
         __graph.set_entry_point("plan_node")
         self.graph = __graph.compile()
         self.__model = model
@@ -37,18 +37,25 @@ class Graph:
 
     def __plan(self, state: GraphState):
         try:
+            screenshot = ImageGrab.grab()
+            screenshot.save("screenshot.jpeg")
+            screenshot.close()
+            print("screenshot saved")
+            with open("screenshot.jpeg", "rb") as image_file:
+                image = base64.b64encode(image_file.read()).decode('utf-8')
+                image_file.close()
             print(f"Task: {state['task']}")
-            message = self.__model.with_structured_output(Plan).invoke(self.__nodes.plan_node(state["task"]))
+            message = self.__model.with_structured_output(Plan).invoke(self.__nodes.plan_node(state["task"], image))
             print(f"Plan: {message.as_str}")
             return {"messages" : [message.as_str], "plan" : message}
-        except Exception:
+        except RateLimitError:
             time.sleep(20)
             self.__plan(state)
 
     def __agent(self, state: GraphState):
         print("in agent node")
         __tools = Mouse().return_tools() + Keyboard().return_tools()
-        __agent = Agent(__tools, self.__nodes.agent_message(Screen().get_size()))
+        __agent = Agent(__tools)
         screenshot = ImageGrab.grab()
         screenshot.save("screenshot.jpeg")
         screenshot.close()
@@ -57,17 +64,8 @@ class Graph:
             image = base64.b64encode(image_file.read()).decode('utf-8')
             image_file.close()
         print("agent created, now invoking")
-        __messages = __agent.graph.invoke({"messages" : [
-            HumanMessage(
-                content=[
-                    {"type" : "text", "text" : f"step to be performed: {state['plan'].plan[0].step}"},
-                    {"type" : "image_url",
-                    "image_url" : {"url" : f"data:image/jpeg;base64,{image}"}},
-                ]
-            )
-        ]})
-        step = state["plan"].plan.pop(0)
-        return {"messages" : __messages["messages"], "steps_completed" : [step.step]}
+        __messages = __agent.graph.invoke({"messages" : self.__nodes.agent_message(Screen().get_size(), state["plan"], image)})
+        return {"messages" : __messages["messages"]}
     
     def __replan(self, state: GraphState):
         screenshot = ImageGrab.grab()
@@ -76,7 +74,11 @@ class Graph:
         with open("screenshot.jpeg", "rb") as image_file:
             image = base64.b64encode(image_file.read()).decode('utf-8')
         print("last llm message: ", state["messages"][-1].content)
-        __replan = self.__model.with_structured_output(Replan).invoke(self.__nodes.replan_node(state["task"], state["plan"].as_str, image, state["messages"][-1].content))
+        try:
+            __replan = self.__model.with_structured_output(Replan).invoke(self.__nodes.replan_node(state["task"], state["plan"].as_str, image, state["messages"][-1].content))
+        except RateLimitError:
+            time.sleep(20)
+            __replan = self.__model.with_structured_output(Replan).invoke(self.__nodes.replan_node(state["task"], state["plan"].as_str, image, state["messages"][-1].content))
         if isinstance(__replan.replan, bool):
             print(f"Replan: {__replan.replan}")
             return {"replan" : __replan.replan}
@@ -85,10 +87,11 @@ class Graph:
             return {"replan" : __replan.replan, "plan" : __replan.replan}
         
     def __check_end(self, state: GraphState):
+        print("Current Plan: ", state["plan"].as_str)
         if isinstance(state["replan"], bool):
-            return not state["replan"]
+            return True
         else:
             if len(state["plan"].plan) == 0:
-                return False
-            else:
                 return True
+            else:
+                return False
