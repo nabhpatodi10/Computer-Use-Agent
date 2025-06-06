5bc7ef515383e7ebba9c80f73bf29ec90c9128bc
nabhpatodi10 <nabhpatodi1005@gmail.com>
2025-06-07 01:33:28 +0530

    Refactor Agent class to use ChatOpenAI; remove system_message parameter and update __call_llm method to handle ToolMessage content. Add screenshot functionality to __take_action method, encoding image as base64 for message response.

--- agent.py
+++ agent.py
@@ -1,15 +1,19 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openai import ChatOpenAI
 from langgraph.graph import StateGraph, END
-from langchain_core.messages import AnyMessage, SystemMessage, ToolMessage
+from langchain_core.messages import AnyMessage, ToolMessage
 from typing import Annotated, TypedDict
 import operator
+import time
+from openai import RateLimitError
+from PIL import ImageGrab
+import base64
 
 class AgentState(TypedDict):
     messages: Annotated[list[AnyMessage], operator.add]
 
 class Agent:
 
-    def __init__(self, tools: list, system_message: list[SystemMessage], model: ChatGoogleGenerativeAI = ChatGoogleGenerativeAI(model = "models/gemini-2.0-flash")):
+    def __init__(self, tools: list, model: ChatOpenAI = ChatOpenAI(model = "gpt-4.1-mini")):
 
         __graph = StateGraph(AgentState)
         __graph.add_node("llm", self.__call_llm)
@@ -22,23 +26,27 @@ class Agent:
         __graph.add_edge("action", "llm")
         __graph.set_entry_point("llm")
         self.graph = __graph.compile()
-        self.__system_message = system_message
         self.__tools = {t.name: t for t in tools}
         self.__model = model.bind_tools(tools)
 
     def __call_llm(self, state: AgentState):
         try:
-            messages = self.__system_message + state["messages"]
-            message = self.__model.invoke(messages)
-            for m in state["messages"]:
-                if isinstance(m, ToolMessage):
-                    state["messages"].remove(m)
+            message = self.__model.invoke(state["messages"])
+            for m in range(len(state["messages"])):
+                if isinstance(state["messages"][m], ToolMessage):
+                    state["messages"][m].content = ""
             print(f"LLM: {message.content}")
             return {"messages" : [message]}
-        except Exception as error:
-            # time.sleep(20)
-            # self.__call_llm(state)
-            raise error
+        except RateLimitError as error:
+            print("Got error\n", error)
+            print("retrying...")
+            time.sleep(20)
+            message = self.__model.invoke(state["messages"])
+            for m in range(len(state["messages"])):
+                if isinstance(state["messages"][m], ToolMessage):
+                    state["messages"][m].content = ""
+            print(f"LLM: {message.content}")
+            return {"messages" : [message]}
     
     def __take_action(self, state: AgentState):
         try:
@@ -52,6 +60,18 @@ class Agent:
                     result = self.__tools[t["name"]].invoke(t["args"])
                 results.append(ToolMessage(tool_call_id = t["id"], name = t["name"], content = str(result)))
             print("Back to model!")
+            screenshot = ImageGrab.grab()
+            screenshot.save("screenshot.jpeg")
+            screenshot.close()
+            print("screenshot saved")
+            with open("screenshot.jpeg", "rb") as image_file:
+                image = base64.b64encode(image_file.read()).decode('utf-8')
+                image_file.close()
+            results[-1].content = [
+                {"type" : "text", "text" : f"{results[-1].content}\n\nAlso, the following is the screenshot of the screen after performing all the previous actions:\n"},
+                {"type" : "image_url",
+                "image_url" : {"url" : f"data:image/jpeg;base64,{image}"}}
+            ]
             return {"messages" : results}
         except Exception as error:
             print(t)
